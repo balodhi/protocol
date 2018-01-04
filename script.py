28c646604ed13f7f0b701ab08b3cc3ad059f5284
Zeeshan Nasim <znasim09@Zeeshans-Mac-Pro.local>
2018-01-04 16:45:00 +0900

    python script

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import os as sys
 import time as time
-command = ""
+import numpy as np
+
+command = "ls"
 timeDiff = []
 for i in range(3):
 	start_time = time.time()
@@ -8,3 +10,10 @@ for i in range(3):
 	end_time = time.time()
 	elapsed_time.append(end_time-start_time)
 	print("Time Elapsed for the command:",elapsed_time[i]
+
+#timed=np.array(timeDiff)
+stdev = np.std(timeD)
+meanT = np.mean(timeD)
+
+print ("Standard Deviation of execuation:", stdev)
+print ("Mean of execuation:", meanT)
